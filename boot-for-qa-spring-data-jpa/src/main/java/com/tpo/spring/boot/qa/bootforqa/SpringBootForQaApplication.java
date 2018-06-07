f92de80969637ace7035ea89e16e2276c9c34412
Tibi <tiberiu.popa@gmail.com>
2018-06-07 13:47:24 +0200

    Initial commit

---
+++ boot-for-qa-spring-data-jpa/src/main/java/com/tpo/spring/boot/qa/bootforqa/SpringBootForQaApplication.java
@@ -0,0 +1,12 @@
+package com.tpo.spring.boot.qa.bootforqa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootForQaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootForQaApplication.class, args);
+	}
+}
