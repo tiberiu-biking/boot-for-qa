8baac2b39224164ad2b2476b49eac0f8e6dbdac5
Tibi <tiberiu.popa@gmail.com>
2018-06-22 13:10:51 +0200

    Fine tunning

--- boot-for-qa-spring-data-jpa-cache/src/main/java/com/tpo/spring/boot/qa/bootforqa/cache/jpa/CachedPersistenceConfiguration.java
+++ boot-for-qa-spring-data-jpa-cache/src/main/java/com/tpo/spring/boot/qa/bootforqa/cache/jpa/CachedPersistenceConfiguration.java
@@ -6,14 +6,12 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
  * @author Tiberiu Popa
  * @since 6/7/2018
  */
 @Configuration
-@EnableTransactionManagement
 @EntityScan(basePackageClasses = Transaction.class)
 @EnableJpaRepositories(basePackageClasses = CachedTransactionRepository.class)
 @EnableCaching
